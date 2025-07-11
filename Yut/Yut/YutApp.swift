dd374937e397254cc56937f9700eff61611b06ca
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-07-11 10:04:00 +0900

    feat: 레포 생성 및 깃 초기 세팅

---
+++ Yut/Yut/YutApp.swift
@@ -0,0 +1,17 @@
+//
+//  YutApp.swift
+//  Yut
+//
+//  Created by Jay on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct YutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
