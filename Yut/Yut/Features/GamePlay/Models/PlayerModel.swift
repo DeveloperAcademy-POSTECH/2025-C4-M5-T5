a91b002bec2e2426c28987ea8e42409349fe0493
sep037 <tmddms0307@dankook.ac.kr>
2025-07-21 08:18:00 +0100

    feat: 플레이어 순서 추가 #28

--- Yut/Yut/Features/GamePlay/Models/PlayerModel.swift
+++ Yut/Yut/Features/GamePlay/Models/PlayerModel.swift
@@ -5,16 +5,18 @@
 //  Created by Seungeun Park on 7/17/25.
 //
 
-class YutPlayer {
+class PlayerModel {
     let name: String
-    var tokens: [Piece]
+    var tokens: [PieceModel]
+    var sequence: Int
     
-    init(name: String){
+    init(name: String, sequence: Int){
         self.name = name
         self.tokens = []
+        self.sequence = sequence
         
         for _ in 0..<2{
-            let token = Piece(owner: self)
+            let token = PieceModel(owner: self)
             self.tokens.append(token)
         }
     }
