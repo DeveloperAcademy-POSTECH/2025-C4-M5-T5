3427abc5b45d47b243463e2f9305eb9594159dda
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-07-24 02:31:10 +0900

    feat: 윷 모델 추가

--- Yut/Yut/Features/Yut/Model/YutModel.swift
+++ Yut/Yut/Features/Yut/Model/YutModel.swift
@@ -1,8 +1,8 @@
-//import RealityKit
-//import Foundation
-//
-//struct YutModel {
-//    let id: UUID = UUID()     // 식별용
-//    let entity: ModelEntity   // 실제 RealityKit 상의 윷
-//    var isFrontUp: Bool?      // 결과 판단 후 저장
-//}
+import RealityKit
+import Foundation
+
+struct YutModel {
+    let id: UUID = UUID()     // 식별용
+    let entity: ModelEntity   // 실제 RealityKit 상의 윷
+    var isFrontUp: Bool?      // 결과 판단 후 저장
+}
