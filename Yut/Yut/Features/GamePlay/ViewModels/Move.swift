e8fdd2fc1a1d7597b0469ade87ad58caa3383852
sep037 <tmddms0307@dankook.ac.kr>
2025-07-23 09:00:24 +0100

    fix: 방향, 칸 더하기 로직 삭제 #28

--- Yut/Yut/Features/GamePlay/ViewModels/Move.swift
+++ Yut/Yut/Features/GamePlay/ViewModels/Move.swift
@@ -8,40 +8,26 @@
 import simd
 
 struct MoveResult {
+    let piece: PieceModel
     let cell: BoardCellModel
     let id: String
-}
-
-enum Direction {
-    case up, left, down, right
-    case diagonalRight, diagonalLeft
-
-    var offset: SIMD3<Int> {
-        switch self {
-        case .up: return SIMD3(-1, 0, 0)
-        case .left: return SIMD3(0, -1, 0)
-        case .down: return SIMD3(1, 0, 0)
-        case .right: return SIMD3(0, 1, 0)
-        case .diagonalLeft: return SIMD3(1, -1, 0)
-        case .diagonalRight: return SIMD3(1, 1, 0)
-
-        }
-    }
+    var didCapture: Bool = false
+    var didCarry: Bool = false
+    var didGoal: Bool = false
 }
 
 extension BoardModel {
-    func move(from cell: BoardCellModel, direction: Direction, steps: Int) -> MoveResult? {
-            let offset = direction.offset &* steps
-            let newRow = cell.row + offset.x
-            let newCol = cell.col + offset.y
-
-            guard (0..<size).contains(newRow), (0..<size).contains(newCol),
-                  let nextCell = cellAt(row: newRow, col: newCol) else {
+    func move(from cell: BoardCellModel, steps: Int) -> BoardCellModel? {
+        var current = cell
+        for _ in 0..<steps {
+            guard let nextCoords = current.nextCandidates.first,
+                  let nextCell = cellAt(row: nextCoords.row, col: nextCoords.col) else {
                 return nil
             }
-
-            return MoveResult(cell: nextCell, id: nextCell.id)
+            current = nextCell
         }
+        return current
+    }
 
     func cellAt(row: Int, col: Int) -> BoardCellModel? {
         return cells.first { $0.row == row && $0.col == col }
