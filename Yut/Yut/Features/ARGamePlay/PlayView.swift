9ebfd57d7dfeeb22717d312c8f2320bcc3fcbd66
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-07-31 03:23:03 +0900

    fix: 효과음 이름 변경

--- Yut/Yut/Features/ARGamePlay/PlayView.swift
+++ Yut/Yut/Features/ARGamePlay/PlayView.swift
@@ -161,7 +161,7 @@ struct PlayView: View {
                                 showFinalFrame = false // 최종 프레임 숨김 (겹침 방지용)
                                 
                                 // 2. 효과음 재생
-                                sound.playcollectYutSound()
+                                sound.playCollectYutSound()
                                 
                                 // 3. 약간의 지연 후, 실제 윷 던지기 시작
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
@@ -176,10 +176,7 @@ struct PlayView: View {
                     }.onAppear {
                         // 윷 던지기 준비 상태
                         arState.showThrowButton = true
-                        
-
                     }
-                    
                     
                     // 5.5 윷 던지기 결과 표시
                 case .showingYutResult:
